e52489f83ed693857160bfae5aabb74acf5617b4
Song Cui <cs-inter@163.com>
2013-01-20 16:22:08 -0500

    add the form for request check and deposit check

---
+++ task7/src/form/DepositCheckForm.java
@@ -0,0 +1,40 @@
+package form;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DepositCheckForm extends FormBean {
+	private String customerName;
+	private String amount;
+	
+    public String getAmount() { return amount;}
+	public String getCustomerName() {return customerName;}
+	
+	public void setCustomerName(String s)   { customerName = s.trim();} 
+	public void setAmount(String s)         { amount = s.trim(); }
+	
+	public List<String> getValidationErrors() {
+		List<String> errors = new ArrayList<String>();
+		double x=0;
+		
+		if (customerName == null || customerName.length() == 0) {
+			errors.add("customer name is required");
+		}
+		
+		if (amount == null || amount.length() == 0) {
+			errors.add("Amount is required");
+		}
+		
+		try {
+			 x = Double.parseDouble(amount);
+		} catch ( NumberFormatException e1) {
+			errors.add("You should enter a number");
+		}
+		
+		if(x<0) {
+			errors.add("The amount should be positive");
+		}
+			
+		return errors;
+	}
+
+}
---
+++ task7/src/form/RequestCheckForm.java
@@ -0,0 +1,35 @@
+package form;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RequestCheckForm extends FormBean {
+	private String amount;
+	
+	public String getAmount() { return amount;}
+	
+	public void setAmount(String s) { amount = s.trim(); }
+	
+	public List<String> getValidationErrors() {
+		List<String> errors = new ArrayList<String>();
+		double x=0;
+		
+		if (amount == null || amount.length() == 0) {
+			errors.add("Amount is required");
+			return errors;
+		}
+		
+		try {
+			 x = Double.parseDouble(amount);
+		} catch ( NumberFormatException e1) {
+			errors.add("You should enter a number");
+		}
+		
+		if(x<0) {
+			errors.add("The amount should be positive");
+		}
+			
+		return errors;
+		
+		}
+
+}
