b875553f1ab90cca67639acf4fda296029542c42
chen04yang <chen04yang@gmail.com>
2013-01-20 23:48:44 -0500

    All dao up to date

--- task7/src/dao/TransactionDao.java
+++ task7/src/dao/TransactionDao.java
@@ -2,8 +2,10 @@ package dao;
 
 import java.util.List;
 
+import model.Customer;
+
 public interface TransactionDao {
 	//Get the amount of the pending transaction of check request and buy funds.
-	public List<Double> getAmountForCheckAndBuy(int customer_id);
+	public List<Double> getAmountForCheckAndBuyByCustomer(Customer c);
 	
 }
--- task7/src/dao/impl/TransactionDaoImpl.java
+++ task7/src/dao/impl/TransactionDaoImpl.java
@@ -1,18 +1,32 @@
 package dao.impl;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import model.Customer;
+import model.Transaction;
 
 import dao.TransactionDao;
 
 public class TransactionDaoImpl extends HibernateBaseDaoImpl implements TransactionDao {
 
 	@Override
-	public List<Double> getAmountForCheckAndBuy(int customer_id) {
-		List<Object> list = this.findByHQL("from Customer c where c.username = '" + username + "'");
-		if(list.size() > 0) {
-			return true;
+	public List<Double> getAmountForCheckAndBuyByCustomer(Customer c) {
+		List<Object> list1 = this.findByHQL("from Transaction t where t.customer = '" + c
+				+ "' and t.execute_date = '" + null
+				+ "' and t.transaction_type = '" + Transaction.CHECK + "'");
+		List<Object> list2 = this.findByHQL("from Transaction t where t.customer = '" + c
+				+ "' and t.execute_date = '" + null
+				+ "' and t.transaction_type = '" + Transaction.BUY + "'");
+		list1.addAll(list2);
+		if(list1.size() == 0) {
+			return null;
 		}
-		return false;
+		List<Double> amountList = new ArrayList<Double>();
+		for(Object o : list1) {
+			amountList.add(((Transaction) o).getAmount());
+		}
+		return amountList;
 	}
 
 }
